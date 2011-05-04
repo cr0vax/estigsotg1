63a99036bb92e5181d151ac32d5f3eede41215ce
bruno.as.moreira <bruno.as.moreira@gmail.com>
2011-05-04 20:57:56 +0000

    Versão com leitura de ficheiros completa

--- JogoSO/JogoSO/JogoSO.cpp
+++ JogoSO/JogoSO/JogoSO.cpp
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <ctype.h>
 
 #define MAX_CELULAS 9
 
@@ -84,6 +85,89 @@ void inicializa_jogador(struct Jogador *pJogador, char* nome, bool blnSuperUser)
 	printf( "Flag:%d\n", pJogador->flg_tem_tesouro );
 }
 
+// ltrim
+char *ltrim(char *s)
+{
+    while(isspace(*s)) s++;
+    return s;
+}
+
+// rtrim
+char *rtrim(char *s)
+{
+    char* back = s + strlen(s);
+    while(isspace(*--back));
+    *(back+1) = '\0';
+    return s;
+}
+
+// trim
+char *trim(char *s)
+{
+    return rtrim(ltrim(s)); 
+}
+
+// TODO: inicilializa mapa com base num ficheiro
+void inicializa_mapa_ficheiro(struct Celula pMapa[], char* pFicheiroMapa)
+{
+	#define MAX_LIN 80
+	#define CAMPOS 7
+
+	FILE *f;
+	char l[ MAX_LIN ];
+
+	f = fopen( pFicheiroMapa, "r" );
+	
+	// inicia a linha
+	int iLinha = 0;
+
+	// inicia o contador
+	int iIndice = -1;
+
+	// percorre o ficheiro até encontrar o fim
+	while( fgets(l, MAX_LIN, f) != NULL ){
+
+		// se o resto da divisão do número da linha pelo número de linhas por célula
+		// for igual a 0, quer dizer que é um novo registo
+		if (iLinha % CAMPOS == 0)
+		{
+			// incrementa o indice
+			iIndice = iIndice + 1;
+		}
+
+		// imprime a linha
+		int iResto = iLinha % CAMPOS;
+
+		switch ( iResto )
+		{
+			case 0:	// Descrição
+				strcpy(pMapa[iIndice].descricao, rtrim(l));
+				break;
+			case 1:	// Norte
+				pMapa[iIndice].norte = atoi (l);
+				break;
+			case 2:	// Sul
+				pMapa[iIndice].sul = atoi (l);
+				break;
+			case 3:	// Este
+				pMapa[iIndice].este = atoi (l);
+				break;
+			case 4:	// Oeste
+				pMapa[iIndice].oeste = atoi (l);
+				break;
+			case 5:	// Item
+				pMapa[iIndice].item = atoi (l);
+				break;
+		}
+
+		// incrementa o número da linha
+		iLinha = iLinha + 1;
+	}
+	
+	fclose( f );
+
+}
+
 //Inicializa o mapa da aventura
 //--------------------------------
 //    +------+------+------+
@@ -96,104 +180,114 @@ void inicializa_jogador(struct Jogador *pJogador, char* nome, bool blnSuperUser)
 //    |                    |
 //    |6     |7     |8     |
 //    +------+------+------+
-void inicializa_mapa(struct Celula pMapa[])
+void inicializa_mapa(struct Celula pMapa[], char* pFicheiroMapa)
 {
-	int n;
 
-	//pMapa[0].este = 1;
-	//pMapa[0]->norte = 1;
-	//printf("[MAPA] Sala 0 %d", pMapa[0].este);
+	// se o tamanho da string pFicheiroMapa for superior a 0 então é
+	// para carregar o mapa do ficheiro
+	if (strlen(pFicheiroMapa) > 1)
+	{
+		printf("Inicializa Mapa %d\n", strlen(pFicheiroMapa));
+		// carrega mapa do ficheiro
+		inicializa_mapa_ficheiro(pMapa, pFicheiroMapa);
+	}
+	else
+	{
+		//Construção da sala 0
+		//----------------------
+		strcpy( pMapa[0].descricao, "Sala 0");
+		pMapa[0].norte	= -1;
+		pMapa[0].sul	= -1;
+		pMapa[0].este	= 1;
+		pMapa[0].oeste	= -1;
+		pMapa[0].item	= -1;
 
-	//Construção da sala 0
-	//----------------------
-	strcpy( pMapa[0].descricao, "Sala 0");
-	pMapa[0].norte	= -1;
-	pMapa[0].sul	= -1;
-	pMapa[0].este	= 1;
-	pMapa[0].oeste	= -1;
-	pMapa[0].item	= -1;
+		//Construção da sala 1
+		//----------------------
+		strcpy( pMapa[1].descricao, "Sala 1");
+		pMapa[1].norte	= -1;
+		pMapa[1].sul	= 4;
+		pMapa[1].este	= -1;
+		pMapa[1].oeste	= 0;
+		pMapa[1].item	= -1;
 
-	//Construção da sala 1
-	//----------------------
-	strcpy( pMapa[1].descricao, "Sala 1");
-	pMapa[1].norte	= -1;
-	pMapa[1].sul	= 4;
-	pMapa[1].este	= -1;
-	pMapa[1].oeste	= 0;
-	pMapa[1].item	= -1;
+		//Construção da sala 2
+		//----------------------
+		strcpy( pMapa[2].descricao, "Sala 2");
+		pMapa[2].norte	= -1;
+		pMapa[2].sul	= 5;
+		pMapa[2].este	= -1;
+		pMapa[2].oeste	= -1;
+		pMapa[2].item	= -1;
 
-	//Construção da sala 2
-	//----------------------
-	strcpy( pMapa[2].descricao, "Sala 2");
-	pMapa[2].norte	= -1;
-	pMapa[2].sul	= 5;
-	pMapa[2].este	= -1;
-	pMapa[2].oeste	= -1;
-	pMapa[2].item	= -1;
+		//Construção da sala 3
+		//----------------------
+		strcpy( pMapa[3].descricao, "Sala 3");
+		pMapa[3].norte	= -1;
+		pMapa[3].sul	= 6;
+		pMapa[3].este	= 4;
+		pMapa[3].oeste	= -1;
+		pMapa[3].item	= -1;
 
-	//Construção da sala 3
-	//----------------------
-	strcpy( pMapa[3].descricao, "Sala 3");
-	pMapa[3].norte	= -1;
-	pMapa[3].sul	= 6;
-	pMapa[3].este	= 4;
-	pMapa[3].oeste	= -1;
-	pMapa[3].item	= -1;
+		//Construção da sala 4
+		//----------------------
+		strcpy( pMapa[4].descricao, "Sala 4");
+		pMapa[4].norte	= 1;
+		pMapa[4].sul	= -1;
+		pMapa[4].este	= 5;
+		pMapa[4].oeste	= 3;
+		pMapa[4].item	= -1;
 
-	//Construção da sala 4
-	//----------------------
-	strcpy( pMapa[4].descricao, "Sala 4");
-	pMapa[4].norte	= 1;
-	pMapa[4].sul	= -1;
-	pMapa[4].este	= 5;
-	pMapa[4].oeste	= 3;
-	pMapa[4].item	= -1;
+		//Construção da sala 5
+		//----------------------
+		strcpy( pMapa[5].descricao, "Sala 5");
+		pMapa[5].norte	= 2;
+		pMapa[5].sul	= -1;
+		pMapa[5].este	= -1;
+		pMapa[5].oeste	= 4;
+		pMapa[5].item	= 0;
 
-	//Construção da sala 5
-	//----------------------
-	strcpy( pMapa[5].descricao, "Sala 5");
-	pMapa[5].norte	= 2;
-	pMapa[5].sul	= -1;
-	pMapa[5].este	= -1;
-	pMapa[5].oeste	= 4;
-	pMapa[5].item	= 0;
+		//Construção da sala 6
+		//----------------------
+		strcpy( pMapa[6].descricao, "Sala 6");
+		pMapa[6].norte	= 3;
+		pMapa[6].sul	= -1;
+		pMapa[6].este	= 7;
+		pMapa[6].oeste	= -1;
+		pMapa[6].item	= -1;
 
-	//Construção da sala 6
-	//----------------------
-	strcpy( pMapa[6].descricao, "Sala 6");
-	pMapa[6].norte	= 3;
-	pMapa[6].sul	= -1;
-	pMapa[6].este	= 7;
-	pMapa[6].oeste	= -1;
-	pMapa[6].item	= -1;
+		//Construção da sala 7
+		//----------------------
+		strcpy( pMapa[7].descricao, "Sala 7");
+		pMapa[7].norte	= -1;
+		pMapa[7].sul	= -1;
+		pMapa[7].este	= 8;
+		pMapa[7].oeste	= 6;
+		pMapa[7].item	= -1;
 
-	//Construção da sala 7
-	//----------------------
-	strcpy( pMapa[7].descricao, "Sala 7");
-	pMapa[7].norte	= -1;
-	pMapa[7].sul	= -1;
-	pMapa[7].este	= 8;
-	pMapa[7].oeste	= 6;
-	pMapa[7].item	= -1;
+		//Construção da sala 8
+		//----------------------
+		strcpy( pMapa[8].descricao, "Sala 8");
+		pMapa[8].norte	= -1;
+		pMapa[8].sul	= -1;
+		pMapa[8].este	= -1;
+		pMapa[8].oeste	= 7;
+		pMapa[8].item	= -1;
+	}
+}
 
-	//Construção da sala 8
-	//----------------------
-	strcpy( pMapa[8].descricao, "Sala 8");
-	pMapa[8].norte	= -1;
-	pMapa[8].sul	= -1;
-	pMapa[8].este	= -1;
-	pMapa[8].oeste	= 7;
-	pMapa[8].item	= -1;
-
-	// Valida MAPA
-	//for (int i = 0; i < MAX_CELULAS; i++) {
-	//	printf("[%s]\n", pMapa[i].descricao);
-	//	printf("Norte: %d\n", pMapa[i].norte);
-	//	printf("Sul:   %d\n", pMapa[i].sul);
-	//	printf("Este:  %d\n", pMapa[i].este);
-	//	printf("Oeste: %d\n", pMapa[i].oeste);
-	//	printf("Item:  %d\n\n", pMapa[i].item);
-	//}
+// testes da inicialização do mapa
+void inicializa_mapa_teste (struct Celula pMapa[])
+{
+	for (int i = 0; i < MAX_CELULAS; i++)
+	{
+		printf("[%s]\n", pMapa[i].descricao);
+		printf("Norte: %d\n", pMapa[i].norte);
+		printf("Sul:   %d\n", pMapa[i].sul);
+		printf("Este:  %d\n", pMapa[i].este);
+		printf("Oeste: %d\n", pMapa[i].oeste);
+		printf("Item:  %d\n\n", pMapa[i].item);
+	}
 }
 
 // inicializa o monstro
@@ -257,26 +351,28 @@ void movimenta_monstro(struct Monstro *pMonstro, struct Celula pMapa[])
 }
 
 // Aceita comando do jogador
-int aceita_comando_jogador(char* *sComando, struct Jogador *pJogador, struct Celula pMapa[])
+int aceita_comando_jogador(char *sComando, struct Jogador *pJogador, struct Celula pMapa[])
 {
 	// valida a localização do jogador
 	int iLocalizacaoJogador = pJogador->localizacao;
 	int iMovimento = -1;
 
 	// validar comandos disponíveis
-	if ( strcmp((char*) sComando, "N") == 0 || strcmp((char*) sComando, "n") == 0 ) {
+	sComando = strupr(sComando);
+
+	if ( strcmp(sComando, "N") == 0 ) {
 		iMovimento = pMapa[iLocalizacaoJogador].norte;
 	}
 	
-	if ( strcmp((char*) sComando, "S") == 0 || strcmp((char*) sComando, "s") == 0 ) {
+	if ( strcmp(sComando, "S") == 0 ) {
 		iMovimento = pMapa[iLocalizacaoJogador].sul;
 	}
 	
-	if ( strcmp((char*) sComando, "E") == 0 || strcmp((char*) sComando, "e") == 0 ) {
+	if ( strcmp(sComando, "E") == 0 ) {
 		iMovimento = pMapa[iLocalizacaoJogador].este;
 	}
 	
-	if ( strcmp((char*) sComando, "O") == 0 || strcmp((char*) sComando, "o") == 0 ) {
+	if ( strcmp(sComando, "O") == 0 ) {
 		iMovimento = pMapa[iLocalizacaoJogador].oeste;
 	}
 
@@ -329,7 +425,6 @@ void descreve_status(struct Jogador *pJogador, struct Monstro *pMonstro, struct 
 	printf("| %s\n", pMapa[pJogador->localizacao].descricao);
 	printf("+------------------------------\n");
 
-	//printf("\n\n\n\n\n\n\n\n\n\n\n\n");
 	printf("\n\n\n\n\n\n");
 	
 }
@@ -462,11 +557,10 @@ void validaSwitches(int argc, char* args[], bool *pSuperUser, char *pFicheiroMap
 {
 	bool godMode = false;
 
-	printf("Argumentos %d", argc);
 	for (int i = 1; i < argc; i++)
 	{
-		if (strcmp(args[i], "su") == 0 || strcmp(args[i], "SU") == 0) { *pSuperUser = true; }			   // modo GOD
-		if (strcmp(args[i], "m") == 0 || strcmp(args[i], "M") == 0) { strcpy(pFicheiroMapa, args[i+1]); }  // faz load do mapa
+		if ( strcmp(strupr(args[i]), "SU") == 0 ) { *pSuperUser = true; }			     // modo SuperUser
+		if ( strcmp(strupr(args[i]), "M") == 0  ) { strcpy(pFicheiroMapa, args[i+1]); }  // faz load do mapa
 	}
 }
 
@@ -481,12 +575,15 @@ int main(int argc, char* argv[])
 	srand(time(0));
 
 	//--------------------------------
-	/Chama a função para validar os switches
+	//Chama a função para validar os switches
 	//--------------------------------
 	bool superUser = false;						// define se está ou não no modo superuser
 	char* ficheiroMapa[100];					// nome do ficheiro do mapa
+	strcpy((char*) ficheiroMapa, "");
 
 	validaSwitches(argc, argv, &superUser, (char*) ficheiroMapa);
+
+	printf("Validou os switches\n");
 
 	//--------------------------------
 	//Chama a função para inicializar o jogador
@@ -503,7 +600,9 @@ int main(int argc, char* argv[])
 	//Chama a função para inicializar o mapa
 	//--------------------------------
 	struct Celula mapa[MAX_CELULAS];				//Cria um array de células para o mapa do jogo
-	inicializa_mapa( mapa,  );
+	inicializa_mapa( mapa, (char *) ficheiroMapa );
+	inicializa_mapa_teste (mapa);
+	system("pause");
 
 	//--------------------------------
 	//Chama a função para inicializar o monstro
@@ -525,15 +624,23 @@ int main(int argc, char* argv[])
 		descreve_status(&jogador, &monstro, mapa);
 
 		//	TODO: Aceitar Comando do Jogador
-		char* sComando;
+		char* sComando[2];
+		strcpy((char*) sComando, "");
 
 		// TODO: validar/imprimir comandos disponíveis
 		char* sComandosDisponiveis = valida_comandos_disponiveis(&mapa[jogador.localizacao]);
 		printf("%s\n", sComandosDisponiveis);
+
+		// solicita comando ao jogador
 		printf("Insira um Comando: ");
-		scanf( "%s", &sComando );
-		int iMovimento = aceita_comando_jogador(&sComando, &jogador, mapa);
-			
+		scanf( "%s", sComando );
+
+		printf("Comando aceite %s\n", sComando);
+
+		int iMovimento = aceita_comando_jogador((char*) sComando, &jogador, mapa);
+		
+		//system("pause");
+		printf ("Aceitou o Comando do Jogador %d\n", iMovimento);
 		if (iMovimento < 0) {
 			printf("Movimento não disponível!\n");
 		}
